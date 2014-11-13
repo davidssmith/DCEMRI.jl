0f49321f329322c0e6ccb262c76e7eeafedf84e8
davidssmith <david.smith@gmail.com>
2014-11-13 11:38:42 -0600

    added run-time analysis

---
+++ rta/rta.jl
@@ -0,0 +1,27 @@
+using PyPlot
+using DCEMRI
+
+rt = readcsv("runtimes.csv")
+nt = float(rt[:,2])
+ft = float(1./rt[:,3])
+
+f(x::Vector{Float64}, a::Vector{Float64}) = a[1] + a[2]*x + a[3]*x.^2
+p0 = [1e-4, 1e-5, 1e-7]
+params, resid = nlsfit(f, ft.'.', [1], nt, p0)
+println("fit params")
+println(params)
+
+figure(1, figsize=(6,5))
+clf()
+loglog(nt, ft, "ko")
+x = logspace(0,log(maximum(nt)), 100)
+loglog(x, f(x, params[:]), "k:", alpha=0.5)
+xlabel("number of time points N in tissue curve")
+ylabel("time to fit one voxel (s)")
+ylim(1e-4,1)
+xlim(10,1e4)
+legend(["measured", @sprintf "%.1eN\$^2\$ + %.1eN + %.1e" params[3] params[2] params[1]], 
+	loc="upper left", frameon=false, numpoints=1, fontsize=10)
+savefig("rta.pdf")
+
+

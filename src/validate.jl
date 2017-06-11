ed61ce3a6fd11e52ba0debb3b3af10b141de9b00
Zaki A <zaki@live.ca>
2017-06-11 08:22:41 -0400

    Consolidate analyze6() and analyze4() into analyzer()

--- src/validate.jl
+++ src/validate.jl
@@ -1,6 +1,6 @@
 using PyPlot
 
-function analyze6(mat::Dict, outdir::AbstractString; dx=1, makeplots=true)
+function analyzer(mat::Dict, outdir::AbstractString; dx=1, makeplots=true, isExt=false)
 
   R1map = mat["R10"]
   S0map = mat["S0"]
@@ -16,14 +16,27 @@ function analyze6(mat::Dict, outdir::AbstractString; dx=1, makeplots=true)
   mask = convert(Array{Bool,2}, mat["mask"])
 
   # compare to known truths
-  u = ones(div(10,dx),div(10,dx))
-  Kt_truth = repmat([0.01*u; 0.02*u; 0.05*u; 0.1*u; 0.2*u; 0.35*u], 1, 5)
-  ve_truth = repmat([0.01*u; 0.05*u; 0.1*u; 0.2*u; 0.5*u]', 6, 1)
+  if ~isExt
+    u = ones(div(10,dx),div(10,dx))
+    Kt_truth = repmat([0.01*u; 0.02*u; 0.05*u; 0.1*u; 0.2*u; 0.35*u], 1, 5)
+    ve_truth = repmat([0.01*u; 0.05*u; 0.1*u; 0.2*u; 0.5*u]', 6, 1)
+  else
+    u = ones(div(180,dx),div(10,dx))
+    Kt_truth = hcat(0.01u, 0.02u, 0.05u, 0.1u, 0.2u)
+
+    u = ones(div(10,dx),div(50,dx))
+    u = vcat(0.1u, 0.2u, 0.5u)
+    ve_truth = vcat(u, u, u, u, u, u)
+
+    u = ones(div(30,dx),div(50,dx))
+    vp_truth = vcat(0.001u, 0.005u, 0.01u, 0.02u, 0.05u, 0.1u)
+  end
 
   Kt_error = clamp.(100.0*(Kt - Kt_truth) ./ (Kt_truth + eps()), -100.0, 100.0)
   ve_error = clamp.(100.0*(ve - ve_truth) ./ (ve_truth + eps()), -100.0, 100.0)
   cccKt = ccc(Kt_truth, Kt)
   cccve = ccc(ve_truth, ve)
+  cccvp = NaN
   print_with_color(:green, "Kt\n\tRMSE:\t$(sqrt(norm(Kt_error)^2 / length(Kt_error))) %\n")
   print_with_color(:green, "\terrmax:\t$(maximum(abs.(Kt_error)))\n")
   print_with_color(:green, "\tCCC:\t$cccKt\n")
@@ -31,14 +44,37 @@ function analyze6(mat::Dict, outdir::AbstractString; dx=1, makeplots=true)
   print_with_color(:green, "\terrmax:\t$(maximum(abs.(ve_error)))\n")
   print_with_color(:green, "\tCCC:\t$cccve\n")
 
-  if !makeplots
-    return (cccKt, cccve)
+  if isExt
+    vp_error = clamp.(100.0*(vp - vp_truth) ./ (vp_truth + eps()), -100.0, 100.0)
+    cccvp = ccc(vp_truth, vp)
+    print_with_color(:green, "vp\n\tRMSE:\t$(sqrt(norm(vp_error)^2 / length(vp_error))) %\n")
+    print_with_color(:green, "\terrmax:\t$(maximum(abs.(vp_error))) %\n")
+    print_with_color(:green, "\tCCC:\t$cccvp\n")
   end
 
-  ytpos = collect((0+floor(Integer, 5/dx)):div(10,dx):(div(60,dx)-1))
-  xtpos = collect((0+floor(Integer, 5/dx)):div(10,dx):(div(50,dx)-1))
-  ytlabels = [string(x) for x in [0.01,0.02,0.05,0.1,0.2,0.35]]
-  xtlabels = [string(x) for x in [0.01,0.05,0.1,0.2,0.5]]
+  if !makeplots
+    return (cccKt, cccve, cccvp)
+  end
+
+  if ~isExt
+    ytpos = collect((0+floor(Integer, 5/dx)):div(10,dx):(div(60,dx)-1))
+    xtpos = collect((0+floor(Integer, 5/dx)):div(10,dx):(div(50,dx)-1))
+    ytlabels = [string(x) for x in [0.01,0.02,0.05,0.1,0.2,0.35]]
+    xtlabels = [string(x) for x in [0.01,0.05,0.1,0.2,0.5]]
+    mapWidth = 4.5
+    mapHeight = 4.5
+    mapLabelX = "\$v_\\mathrm{e}\$"
+    mapLabelY = "\$K^\\mathrm{trans}\$"
+  else
+    ytpos = collect((div(10,dx)+floor(Integer, 5/dx)):div(30,dx):(div(180,dx)-1))
+    xtpos = collect((0+floor(Integer, 5/dx)):div(10,dx):(div(50,dx)-1))
+    ytlabels = [string(x) for x in [0.001, 0.005, 0.01, 0.02, 0.05, 0.1]]
+    xtlabels = [string(x) for x in [0.01,0.02,0.05,0.1,0.2]]
+    mapWidth = 3.5
+    mapHeight = 6
+    mapLabelX = "\$K^\\mathrm{trans}\$"
+    mapLabelY = "\$v_\\mathrm{p}\$"
+  end
 
   println("Plotting results ...")
   # AIF
@@ -46,248 +82,116 @@ function analyze6(mat::Dict, outdir::AbstractString; dx=1, makeplots=true)
   clf()
   plot(mat["t"], mat["Cp"], "ko-")
   xlabel("time (min)")
-  # yticks([0:2:10]) # This produces an error
+  yticks(collect(0:2:10)) # This produces an error
   ylim(0,10)
   ylabel("[Gd-DTPA] (mM)")
   title("arterial input function, \$C_p\$")
-  savefig("$outdir/aif.pdf")
+  savefig("$outdir/aif.pdf",bbox_inches="tight")
 
-  figure(figsize=(4.5, 4.5))
+  figure(figsize=(mapWidth, mapHeight))
   clf()
   imshow(modelmap, interpolation="nearest", cmap="cubehelix")
   title("model used")
   xticks(xtpos, xtlabels)
   yticks(ytpos, ytlabels)
-  xlabel("\$v_\\mathrm{e}\$")
-  ylabel("\$K^\\mathrm{trans}\$")
-  colorbar(ticks=[0,1,2,3])
+  xlabel(mapLabelX)
+  ylabel(mapLabelY)
+  colorbar()
   savefig("$outdir/modelmap.pdf",bbox_inches="tight")
 
   # PARAMETER MAPS
-  figure(figsize=(4.5, 4.5))
+  figure(figsize=(mapWidth, mapHeight))
   clf()
   imshow(Kt, interpolation="nearest", cmap="cubehelix", vmin=0, vmax=0.35)
   title("\$K^\\mathrm{trans}\$ (min\$^{-1}\$)")
   xticks(xtpos, xtlabels)
   yticks(ytpos, ytlabels)
-  xlabel("\$v_\\mathrm{e}\$")
-  ylabel("\$K^\\mathrm{trans}\$")
-  colorbar(ticks=[0.1,0.2,0.3,0.4])
+  xlabel(mapLabelX)
+  ylabel(mapLabelY)
+  colorbar()
   savefig("$outdir/Kt.pdf",bbox_inches="tight")
 
-  figure(figsize=(4.5, 4))
+  figure(figsize=(mapWidth, mapHeight))
   clf()
   imshow(ve, interpolation="nearest", cmap="cubehelix", vmin=0, vmax=0.6)
   title("\$v_\\mathrm{e}\$")
-  colorbar(ticks=[0:6]/10.0)
   xticks(xtpos, xtlabels)
   yticks(ytpos, ytlabels)
-  xlabel("\$v_\\mathrm{e}\$")
-  ylabel("\$K^\\mathrm{trans}\$")
+  xlabel(mapLabelX)
+  ylabel(mapLabelY)
+  colorbar()
   savefig("$outdir/ve.pdf",bbox_inches="tight")
 
-  figure(figsize=(4.5, 4))
+  if isExt
+    figure(figsize=(mapWidth, mapHeight))
+    clf()
+    imshow(vp, interpolation="nearest", cmap="cubehelix", vmin=0, vmax=0.12)
+    title("\$v_p\$")
+    xticks(xtpos, xtlabels, fontsize=8)
+    yticks(ytpos, ytlabels)
+    xlabel(mapLabelX)
+    ylabel(mapLabelY)
+    colorbar()
+    savefig("$outdir/vp.pdf",bbox_inches="tight")
+  end
+
+  figure(figsize=(mapWidth, mapHeight))
   clf()
   imshow(resid, interpolation="nearest", cmap="cubehelix", vmin=0)
   title("residual")
-  colorbar()
   xticks(xtpos, xtlabels)
   yticks(ytpos, ytlabels)
-  xlabel("\$v_\\mathrm{e}\$")
-  ylabel("\$K^\\mathrm{trans}\$")
+  xlabel(mapLabelX)
+  ylabel(mapLabelY)
+  colorbar()
   savefig("$outdir/resid.pdf",bbox_inches="tight")
 
-  figure(figsize=(4.5, 4))
+  figure(figsize=(mapWidth, mapHeight))
   clf()
   m = maximum(abs.(Kt_error))
   imshow(Kt_error, interpolation="nearest", cmap="PiYG", vmin=-m, vmax=m)
   title("% error in \$K^\\mathrm{trans}\$")
   xticks(xtpos, xtlabels)
   yticks(ytpos, ytlabels)
-  xlabel("\$v_\\mathrm{e}\$")
-  ylabel("\$K^\\mathrm{trans}\$")
+  xlabel(mapLabelX)
+  ylabel(mapLabelY)
   colorbar()
   savefig("$outdir/Kt_error.pdf",bbox_inches="tight")
 
-  figure(figsize=(4.5, 4))
+  figure(figsize=(mapWidth, mapHeight))
   clf()
   m = maximum(abs.(ve_error))
   imshow(ve_error, interpolation="nearest", cmap="PiYG", vmin=-m, vmax=m)
   title("% error in \$v_\\mathrm{e}\$")
   xticks(xtpos, xtlabels)
   yticks(ytpos, ytlabels)
-  xlabel("\$v_\\mathrm{e}\$")
-  ylabel("\$K^\\mathrm{trans}\$")
+  xlabel(mapLabelX)
+  ylabel(mapLabelY)
   colorbar()
   savefig("$outdir/ve_error.pdf",bbox_inches="tight")
 
-  return (cccKt, cccve)
-end
-
-
-
-function analyze4(mat::Dict, outdir::AbstractString; dx=1, makeplots=true)
-  R1map = mat["R10"]
-  S0map = mat["S0"]
-  modelmap = mat["modelmap"]
-  Ct = mat["Ct"]
-  Kt = mat["Kt"]
-  ve = mat["ve"]
-  vp = mat["vp"]
-  resid = mat["resid"]
-  q = quantile(S0map[:], 0.99)
-  S0map[S0map .> q] = q
-  back = (S0map - minimum(S0map)) / (maximum(S0map) - minimum(S0map))
-  mask = convert(Array{Bool,2}, mat["mask"])
-
-  # compare to known truths
-  u = ones(div(180,dx),div(10,dx))
-  Kt_truth = hcat(0.01u, 0.02u, 0.05u, 0.1u, 0.2u)
-
-  u = ones(div(10,dx),div(50,dx))
-  u = vcat(0.1u, 0.2u, 0.5u)
-  ve_truth = vcat(u, u, u, u, u, u)
-
-  u = ones(div(30,dx),div(50,dx))
-  vp_truth = vcat(0.001u, 0.005u, 0.01u, 0.02u, 0.05u, 0.1u)
-
-  Kt_error = clamp.(100.0*(Kt - Kt_truth) ./ (Kt_truth + eps()), -100.0, 100.0)
-  ve_error = clamp.(100.0*(ve - ve_truth) ./ (ve_truth + eps()), -100.0, 100.0)
-  vp_error = clamp.(100.0*(vp - vp_truth) ./ (vp_truth + eps()), -100.0, 100.0)
-  cccKt = ccc(Kt_truth, Kt)
-  cccve = ccc(ve_truth, ve)
-  cccvp = ccc(vp_truth, vp)
-  print_with_color(:green, "Kt\n\tRMSE:\t$(sqrt(norm(Kt_error)^2 / length(Kt_error))) %\n")
-  print_with_color(:green, "\terrmax:\t$(maximum(abs.(Kt_error))) %\n")
-  print_with_color(:green, "\tCCC:\t$cccKt\n")
-  print_with_color(:green, "ve\n\tRMSE:\t$(sqrt(norm(ve_error)^2 / length(ve_error))) %\n")
-  print_with_color(:green, "\terrmax:\t$(maximum(abs.(ve_error))) %\n")
-  print_with_color(:green, "\tCCC:\t$cccve\n")
-  print_with_color(:green, "vp\n\tRMSE:\t$(sqrt(norm(vp_error)^2 / length(vp_error))) %\n")
-  print_with_color(:green, "\terrmax:\t$(maximum(abs.(vp_error))) %\n")
-  print_with_color(:green, "\tCCC:\t$cccvp\n")
-
-  if !makeplots
-    return (cccKt, cccve, cccvp)
+  if isExt
+    figure(figsize=(mapWidth, mapHeight))
+    clf()
+    m = maximum(abs.(vp_error))
+    imshow(vp_error, interpolation="nearest", cmap="PiYG", vmin=-m, vmax=m)
+    title("% error in \$v_p\$")
+    xticks(xtpos, xtlabels, fontsize=8)
+    yticks(ytpos, ytlabels)
+    xlabel(mapLabelX)
+    ylabel(mapLabelY)
+    colorbar()
+    savefig("$outdir/vp_error.pdf",bbox_inches="tight")
   end
-
-  ytpos = collect((div(10,dx)+floor(Integer, 5/dx)):div(30,dx):(div(180,dx)-1))
-  xtpos = collect((0+floor(Integer, 5/dx)):div(10,dx):(div(50,dx)-1))
-  ytlabels = [string(x) for x in [0.001, 0.005, 0.01, 0.02, 0.05, 0.1]]
-  xtlabels = [string(x) for x in [0.01,0.02,0.05,0.1,0.2]]
-
-  println("Plotting results ...")
-  # AIF
-  figure(figsize=(4,4))
-  clf()
-  plot(mat["t"], mat["Cp"], "ko-")
-  xlabel("time (min)")
-  #yticks([0:10])
-  ylabel("[Gd-DTPA] (mM)")
-  title("arterial input function, \$C_p\$")
-  savefig("$outdir/aif.pdf",bbox_inches="tight")
-
-  figure(figsize=(3.5, 6))
-  clf()
-  imshow(modelmap, interpolation="nearest", cmap="cubehelix")
-  title("model used")
-  xticks(xtpos, xtlabels, fontsize=8)
-  yticks(ytpos, ytlabels)
-  xlabel("\$K^\\mathrm{trans}\$")
-  ylabel("\$v_\\mathrm{p}\$")
-  colorbar(ticks=[0,1,2,3])
-  savefig("$outdir/modelmap.pdf",bbox_inches="tight")
-
-  # PARAMETER MAPS
-  figure(figsize=(3.5, 6))
-  clf()
-  imshow(Kt, interpolation="nearest", cmap="cubehelix", vmin=0, vmax=0.2)
-  title("\$K^\\mathrm{trans}\$ (min\$^{-1}\$)")
-  xticks(xtpos, xtlabels, fontsize=8)
-  yticks(ytpos, ytlabels)
-  xlabel("\$K^\\mathrm{trans}\$")
-  ylabel("\$v_\\mathrm{p}\$")
-  colorbar()
-  savefig("$outdir/Kt.pdf",bbox_inches="tight")
-
-  figure(figsize=(3.5, 6))
-  clf()
-  imshow(ve, interpolation="nearest", cmap="cubehelix", vmin=0, vmax=0.6)
-  title("\$v_e\$")
-  xticks(xtpos, xtlabels, fontsize=8)
-  yticks(ytpos, ytlabels)
-  xlabel("\$K^\\mathrm{trans}\$")
-  ylabel("\$v_\\mathrm{p}\$")
-  colorbar()
-  savefig("$outdir/ve.pdf",bbox_inches="tight")
-
-  figure(figsize=(3.5, 6))
-  clf()
-  imshow(vp, interpolation="nearest", cmap="cubehelix", vmin=0, vmax=0.12)
-  title("\$v_p\$")
-  xticks(xtpos, xtlabels, fontsize=8)
-  yticks(ytpos, ytlabels)
-  xlabel("\$K^\\mathrm{trans}\$")
-  ylabel("\$v_\\mathrm{p}\$")
-  colorbar()
-  savefig("$outdir/vp.pdf",bbox_inches="tight")
-
-  figure(figsize=(3.5, 6))
-  clf()
-  imshow(resid, interpolation="nearest", cmap="cubehelix", vmin=0)
-  title("residual")
-  xticks(xtpos, xtlabels, fontsize=8)
-  yticks(ytpos, ytlabels)
-  xlabel("\$K^\\mathrm{trans}\$")
-  ylabel("\$v_\\mathrm{p}\$")
-  colorbar()
-  savefig("$outdir/resid.pdf",bbox_inches="tight")
-
-
-  figure(figsize=(3.5, 6))
-  clf()
-  m = maximum(abs.(Kt_error))
-  imshow(Kt_error, interpolation="nearest", cmap="PiYG", vmin=-m, vmax=m)
-  title("% error in \$K^\\mathrm{trans}\$")
-  xticks(xtpos, xtlabels, fontsize=8)
-  yticks(ytpos, ytlabels)
-  xlabel("\$K^\\mathrm{trans}\$")
-  ylabel("\$v_\\mathrm{p}\$")
-  colorbar()
-  savefig("$outdir/Kt_error.pdf",bbox_inches="tight")
-
-  figure(figsize=(3.5, 6))
-  clf()
-  m = maximum(abs.(ve_error))
-  imshow(ve_error, interpolation="nearest", cmap="PiYG", vmin=-m, vmax=m)
-  title("% error in \$v_e\$")
-  xticks(xtpos, xtlabels, fontsize=8)
-  yticks(ytpos, ytlabels)
-  xlabel("\$K^\\mathrm{trans}\$")
-  ylabel("\$v_\\mathrm{p}\$")
-  colorbar()
-  savefig("$outdir/ve_error.pdf",bbox_inches="tight")
-
-  figure(figsize=(3.5, 6))
-  clf()
-  m = maximum(abs.(vp_error))
-  imshow(vp_error, interpolation="nearest", cmap="PiYG", vmin=-m, vmax=m)
-  title("% error in \$v_p\$")
-  xticks(xtpos, xtlabels, fontsize=8)
-  yticks(ytpos, ytlabels)
-  xlabel("\$K^\\mathrm{trans}\$")
-  ylabel("\$v_\\mathrm{p}\$")
-  colorbar()
-  savefig("$outdir/vp_error.pdf",bbox_inches="tight")
 
   return (cccKt, cccve, cccvp)
 end
 
 function analyze(n, mat::Dict, outdir::AbstractString; kwargs...)
   if n == 4
-    return analyze4(mat, outdir; kwargs...)
+    return analyzer(mat, outdir; isExt=true, kwargs...)
   elseif n == 6
-    return analyze6(mat, outdir; kwargs...)
+    return analyzer(mat, outdir; isExt=false, kwargs...)
   end
 end
 
